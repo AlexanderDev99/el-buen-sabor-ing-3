0b2615b3326e0e2bc9bcc7a60d0edfe24ac61eff
EdKyo <eadelacruzt@uce.edu.ec>
2026-01-13 13:14:46 -0500

    Se implemento el modulo de administrador

---
+++ src/main/java/com/elbuensabor/reservas/administrador/data/AdministradorEntity.java
@@ -0,0 +1,17 @@
+package com.elbuensabor.reservas.administrador.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table (name = "administradores")
+public class AdministradorEntity {
+
+    @Id
+    private Integer idAdministrador;
+    private String nombre;
+    private String apellido;
+}
