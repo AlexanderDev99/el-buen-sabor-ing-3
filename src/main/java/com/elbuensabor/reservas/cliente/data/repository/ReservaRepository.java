2cd5e47889dde0b7adafc94c74a5f8d1620e4511
Fabricio Torres <bayrtorresxxx@yahoo.es>
2026-01-14 11:29:27 -0500

    Se agregan dos métodos de consulta a la base de datos. El uno que permite paguinar las consultas y el otro para consultar por el codigo de reserva

--- src/main/java/com/elbuensabor/reservas/cliente/data/repository/ReservaRepository.java
+++ src/main/java/com/elbuensabor/reservas/cliente/data/repository/ReservaRepository.java
@@ -1,9 +1,19 @@
 package com.elbuensabor.reservas.cliente.data.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.elbuensabor.reservas.cliente.data.entities.ReservaEntity;
 
-public interface ReservaRepository extends JpaRepository<ReservaEntity, Integer> {
-    
+public interface ReservaRepository
+        extends JpaRepository<ReservaEntity, Integer> {
+
+    @Query("SELECT r FROM ReservaEntity r WHERE r.reservaId = ?1")
+    public ReservaEntity findByReservaId(String reservaId);
+
+    @Query("SELECT r FROM ReservaEntity r")
+    public List<ReservaEntity> findAllPaging(Pageable pageable);
 }
